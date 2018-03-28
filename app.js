4e12e2ae07931d4f92f01654dcc1b588775248b1
Sahana <sahanakmr32@gmail.com>
2018-03-29 08:11:03 +1100

    fixed error message

--- app.js
+++ app.js
@@ -6,15 +6,11 @@ const bodyParser = require('body-parser');
 const processShowsRoutes = require('./api/routes/processShows');
 
 app.use(morgan('dev'));
-//app.use(bodyParser.json());
+app.use(bodyParser.json());
 
-app.use(bodyParser.json({ type: 'application/json' }))
+//app.use(bodyParser.json({ type: 'application/json' }))
 
-// app.use(bodyParser.json({
-//     type: function() {
-//         return true;
-//     }
-// }));
+
 
 app.use((res, req, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -35,6 +31,8 @@ app.use((req,res,next) =>{
 });
 
 app.use((error,req,res,next) =>{
+
+  console.log(error);
 
   let errorMessage;
   res.status(error.status || 500);
